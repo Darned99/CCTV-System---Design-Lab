27bcd7041829345eef9662af33b41f2ea536a08e
Darned99 <kisielewskibartek99@gmail.com>
2025-01-03 16:43:49 +0100

    Changes

--- src/camera.cpp
+++ src/camera.cpp
@@ -7,6 +7,27 @@
  *
  */
 
+/**
+ * Funkcje / metody do zaimplementowania
+ * 
+ *      initializaeCamera(int index):
+ *          otwiera kamerę za pomocą cv::VideoCapture
+ *          ustawia parametry (rozdzielczość, FPS)
+ *          weryfikuje, czy kamera jest dostępna (isOpened())
+ *          
+ *      streamVideo()
+ *          odczytuje klatki za pomocą read() i wyświetla je w oknie OpenCV (cv::imshow)
+ *          obsługuje przypadki, gdy kamera przestaje dostarczać klatki
+ * 
+ *      handleError()
+ *          loguje błędy związane z brakiem kamery lub jej rozłączeniem
+ *          próbuje ponownie nawiązać połączenie z kamerą
+ * 
+ */
+
+
+
+
 #include <iostream>
 #include "camera.h"
 
---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * Wywołanie funkcji z camera.cpp, aby zainicjować kamerę, wyświetlić strumień wideo i obsłużyć potencjalne błędy.
+ * Ten plik będzie koordynował interakcję użytkownika z modułem kamery
+ */
+
+/**
+ *  Wywołanie initializeCamera() z konfiguracja pliku
+ *  Wywołąnie streamVideo(), obsługa wejść użytkownika (np. klawisz do zakończenia programu)
+ *  Zamykanie kamery i czyszczenie zasobów po zakończeniu programu
+ * 
+ */
+
+
+
+#include <iostream>
+#include "camera.h"
+
+int main() {
+
+    return 0;
+}
+
+
